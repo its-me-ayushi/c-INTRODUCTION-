e589fda31bbc4a5f130c254232e4c27e3857a738
ayushi adya <105603159+its-me-ayushi@users.noreply.github.com>
2022-05-17 22:21:43 +0530

    Create output-1.cpp

---
+++ output-1.cpp
@@ -0,0 +1,18 @@
+ /* What will be the output of the following code ?  */ 
+    
+
+
+
+       #include <iostream>
+       using namespace std;
+       int main(){
+       cout << "career" << "labs";
+       } 
+
+         
+
+                  
+  
+
+                                                      // Correct Answer = careerLabs .
+
