4974ad3f916e50145a3d7eba279d9f3c92d7b98a
ayushi adya <105603159+its-me-ayushi@users.noreply.github.com>
2022-05-20 13:58:00 +0530

    Create output_2.cpp

---
+++ output_2.cpp
@@ -0,0 +1,13 @@
+ /* If statement
+For what values of the x is an if-statement block not executed ? */ 
+       if((1+x) != (1-x)) {
+          cout<<"In if block";
+         }
+ /* info
+
+This problem has only one correct answer
+0 and all negative values
+0 and -1
+0, all negative values, all positive values except 1 */ 
+ 
+ // Correct Answer = 0 ;  
