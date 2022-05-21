cf35ed6abcb5e3d87872ea4253075b35d1930042
ayushi adya <105603159+its-me-ayushi@users.noreply.github.com>
2022-05-21 13:43:17 +0530

    Create Fahrenheit_to_Celsius_Table.cpp

---
+++ Fahrenheit_to_Celsius_Table.cpp
@@ -0,0 +1,32 @@
+ /* Fahrenheit to Celsius Table
+ Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
+Input Format :
+3 integers - S, E and W respectively 
+Output Format :
+Fahrenheit to Celsius conversion table. One line for every Fahrenheit and corresponding Celsius value. The Fahrenheit value and its corresponding Celsius value should be separate by single space.
+Constraints :
+0 <= S <= 90
+S <= E <=  900
+0 <= W <= 80    
+Sample Input 1: 0 
+20  -6
+40  4
+60  15
+80  26
+100 37 // Sample Input 2: 20, 119, 13 // Sample Output 2: 20  -6 33  0  46 ;  */ 
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#include<iostream>
+
+using namespace std ;
+int main(){
+int S , W , E  ,  C ;
+  cin>>S ,  W  , E ;
+  
+  for(S;S<E;S=S+W){
+    (C=5.0/9.0)*(S-32) ;
+    cout<<S << ' ' << C  << endl ;
+    
+    }
+  
+}
